47c7d3db727b57045b34acb9edd43b0a8c8a5898
Danielsio <kingddd301@gmail.com>
2022-07-07 07:13:34 +0300

    Hashing Passwords

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app =  express();
 
@@ -15,8 +15,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 mongoose.connect("mongodb://localhost:27017/userDB")
 
 const userSchema = new mongoose.Schema({email: String, password: String});
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -34,7 +32,7 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req,res)
 {
-  const newUser = new User({email: req.body.username, password: req.body.password});
+  const newUser = new User({email: req.body.username, password: md5(req.body.password)});
   newUser.save(function(err)
   {
     if (err)
@@ -52,7 +50,7 @@ app.post("/register", function(req,res)
 app.post("/login", function(req,res)
 {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({email: username}, function(err, foundUser){
     if (err) {
       console.log(err);
